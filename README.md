ef423de1c471d9f1157e865f1ad5be7724f50025
Mubeen Ali <cmubeenali@gmail.com>
2024-12-05 12:29:00 +0530

    App : initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Ecms Admin Portal
+
+This portal for Admins to view the reports
+
+#### License
+
+MIT
---
+++ ecms_admin_portal/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Ecms Admin Portal",
+			"type": "module",
+			"label": _("Ecms Admin Portal")
+		}
+	]
